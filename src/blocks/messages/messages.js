471310b5b0e10d87ba45562a5c89c7798327da95
miirsery <miiresery.main@gmail.com>
2021-12-06 23:13:05 +0700

    something new

--- src/blocks/messages/messages.js
+++ src/blocks/messages/messages.js
@@ -1,53 +1,65 @@
-let documentDate = document.querySelectorAll('.message__date')
-documentDate.forEach(date => date.innerHTML = getTime())
 
-function getTime() {
-    let today = new Date();
-    hours = today.getHours();
-    minutes = today.getMinutes();
+document.addEventListener('DOMContentLoaded', (e) => {
+    // if (window.location.pathname == '/messages/html')
+    // {  }
+    let documentDate = document.querySelectorAll('.message__date')
+    documentDate.forEach(date => date.innerHTML = getTime())
 
-    if (hours < 10) hours = "0" + hours;
+    function getTime() {
+        let today = new Date();
+        hours = today.getHours();
+        minutes = today.getMinutes();
 
-    if (minutes < 10) minutes = "0" + minutes;
+        if (hours < 10) hours = "0" + hours;
 
-    let time = hours + ":" + minutes;
-    return time;
-}
+        if (minutes < 10) minutes = "0" + minutes;
 
-const userInput = document.querySelector('.message__input')
-const messageContent = document.querySelector('.message__content')
-const userBtn = document.querySelector('.message__btn')
-const messageBottom = document.querySelector('.message__form')
-userBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    sendMessage()
+        let time = hours + ":" + minutes;
+        return time;
+    }
+
+    const userInput = document.querySelector('.message__input')
+    const messageContent = document.querySelector('.message__content')
+    const userBtn = document.querySelector('.message__btn')
+    const messageBottom = document.querySelector('.message__form')
+
+
+    userBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        sendMessage()
+    })
+    // userInput.addEventListener('click', (e) => {
+    //     if (e.which == 13) {
+    //         e.preventDefault()
+    //         sendMessage()
+    //     }
+    // })
+
+    function sendMessage() {
+        if (userInput.value != 0) {
+            messageContent.insertAdjacentHTML('beforeend', generateHtml(userInput.value, getTime()))
+            userInput.value = ''
+            scrollLogic()
+        }
+    }
+
+    function generateHtml(text, date) {
+        return `
+        <div class="message__me new-message">
+            <span>
+                <p class="message__me-text">${text}</p>
+                <p class="message__date">${date}</p>
+            </span>
+        </div>
+        `
+    }
+    function scrollLogic() {
+        let shouldScroll = messageContent.scrollTop + messageContent.clientHeight === messageContent.scrollHeight
+        if (!shouldScroll) scrollToBottom()
+    }
+
+    function scrollToBottom() {
+        messageContent.scrollTop = messageContent.scrollHeight
+    }
+    scrollToBottom()
 })
-userInput.addEventListener('click', (e) => {
-    if (e.which == 13) sendMessage()
-})
-
-function sendMessage() {
-    messageContent.insertAdjacentHTML('beforeend', generateHtml(userInput.value, getTime()))
-    userInput.value = ''
-    scrollLogic()
-}
-
-function generateHtml(text, date) {
-    return `
-    <div class="message__me new-message">
-        <span>
-            <p class="message__me-text">${text}</p>
-            <p class="message__date">${date}</p>
-        </span>
-    </div>
-    `
-}
-function scrollLogic() {
-    let shouldScroll = messageContent.scrollTop + messageContent.clientHeight === messageContent.scrollHeight
-    if (!shouldScroll) scrollToBottom() 
-}
-
-function scrollToBottom() {
-    messageContent.scrollTop = messageContent.scrollHeight
-}
-scrollToBottom()
